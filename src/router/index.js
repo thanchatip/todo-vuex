a0d1f1870bf1c837998c6308b77fa9a84ca8adaa
thanchatip <thanchatip.coe@gmail.com>
2020-03-02 11:54:51 +0700

    :recycle: adjust code

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Todo from '../views/Todo.vue'
 import Home from '../views/Home.vue'
 import Edit from '../views/Edit.vue'
 
@@ -11,11 +10,6 @@ const routes = [
     path: '/',
     name: 'home',
     component: Home
-  },
-  {
-    path: '/todo',
-    name: 'todo',
-    component: Todo
   },
   {
     path: '/create',
